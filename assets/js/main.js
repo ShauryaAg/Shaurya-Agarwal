b2fd993d48fd28de14fd177bca0e57fa79095157
Shaurya <agarwal.lucky.shaurya11@gmail.com>
2021-02-01 00:50:02 +0530

    added OpenSource Contributions

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+window.onload = async () => {
+    const response = await fetch('https://api.github.com/search/issues?q=author%3AShauryaAg+type%3Apr').then(res => res.json())
+    console.log(response)
+
+    const open = document.getElementById('open')
+    const closed = document.getElementById('closed')
+    const items = response['items']
+
+    openContribs = []
+    closedContribs = []
+    items.map((item) => {
+        contrib = `
+        <div class="contrib-item card neumo-shadow mt-4 px-2">
+            <div class="contrib-title">
+                <b>Title:</b> <a href="${item["html_url"]}">${item["title"]}</a>
+            </div>
+            <div class="contrib-no">
+                <b>PR Number:</b><span class="contrib-no">#${item["number"]}</span>
+            </div>
+            <div class="contrib-repo">
+                <strong> Repo:</strong> <a href="${item["html_url"].split('pull')[0]}">${item["url"].split('repos/')[1].split('/issues')[0]}</a>
+            </div>
+        </div>
+    `
+        if (item.state == "open") {
+            openContribs.push(contrib)
+        } else {
+            closedContribs.push(contrib)
+        }
+    })
+
+    openInnerHtml = openContribs.join("\r\n")
+    closedInnerHtml = closedContribs.join("\r\n")
+    open.innerHTML = openInnerHtml
+    closed.innerHTML = closedInnerHtml
+}
